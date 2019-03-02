23a3c3eb163a52784c63cc25532278315c84472a
Pablo Jurado <pjurado85@gmail.com>
2019-03-02 13:18:38 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Typescript Chess Game
+
+> NOTE: Work in progress.
+
+```sh
+# to compile to Javascript
+tsc
+
+# for compiling on changes
+# tsc -w
+```
---
+++ src/main.ts
@@ -0,0 +1,115 @@
+// -----------------------------------------------------------------------------
+// Constants
+// -----------------------------------------------------------------------------
+export const BOARD_DOM = document.getElementById('app');
+
+// names
+const PAWN = 'pawn';
+const ROOK = 'rook';
+const KNIGHT = 'knight';
+const BISHOP = 'bishop';
+const QUEEN = 'queen';
+const KING = 'king';
+
+// pieces
+const B_PAWN = '&#9823;';
+const W_PAWN = '&#9817;';
+const B_ROOK = '&#9820;';
+const W_ROOK = '&#9814;';
+const B_KNIGHT = '&#9822;';
+const W_KNIGHT = '&#9816;';
+const B_BISHOP = '&#9821;';
+const W_BISHOP = '&#9815;';
+const B_QUEEN = '&#9819;';
+const W_QUEEN = '&#9813;';
+const B_KING = '&#9818;';
+const W_KING = '&#9812;';
+
+
+// -----------------------------------------------------------------------------
+// Types
+// -----------------------------------------------------------------------------
+type colorType = 'black' | 'white';
+type pieceType = null | Piece;
+
+// -----------------------------------------------------------------------------
+// Classes
+// -----------------------------------------------------------------------------
+export class Board {
+  row = 8;
+  col = 8;
+  board: Square[][];
+
+  constructor() {
+    this.buildBoard();
+    this.render();
+    console.log(this.board);
+  }
+
+  buildBoard() {
+    var arr = [];
+    for (var i = 0; i < this.row; i++) {
+      var rowArr = [];
+      for (var j = 0; j < this.col; j++) {
+        var color: colorType;
+        if (i % 2 === j % 2) {
+          color = "white";
+        } else {
+          color = "black";
+        }
+        rowArr.push(new Square( null, color));
+      }
+      arr.push(rowArr);
+    }
+    this.board = arr;
+  }
+
+  render() {
+    var boardHTML = this.board.reduce(function(acc, item, index) {
+      var row = ''
+      item.forEach(function (element) {
+        row += `<div class="${element.color}"></div>`;
+      })
+      return acc += `<div data-y="${index}" class="row">${row}</div>`;
+    }, '');
+    BOARD_DOM.innerHTML = boardHTML;
+  }
+}
+
+export class Square {
+  piece: pieceType;
+  color: colorType;
+
+  constructor(piece: pieceType, color: colorType) {
+    this.piece = piece;
+    this.color = color;
+  }
+}
+
+export abstract class Piece {
+  x: number;
+  y: number;
+  color: string;
+  name: string;
+
+  constructor(x, y, color, name) {
+    this.x = x;
+    this.y = y;
+    this.color = color;
+    this.name = name;
+  }
+
+  abstract move() :void;
+}
+
+
+
+class Player {
+
+}
+
+class Game {
+
+}
+
+export const board = new Board();
